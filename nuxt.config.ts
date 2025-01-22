f47bd09241547062ae40224f070da41f7953c04d
RaniumFrontendDev <bharath@ranium.in>
2025-01-22 18:30:51 +0530

    Tailwind Integration done

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: ['~/assets/css/main.css'],
   modules: ['@nuxtjs/storybook']
 })
